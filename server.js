547f32a2a6cfe1721d16be8f72df09f258caee76
John Kim <31109475+jok405@users.noreply.github.com>
2018-02-11 23:47:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,116 @@
 const express = require('express');
-const bodyParser = require("body-parser");
-const path = require("path");
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => {
+	console.log('home page requested');
+	res.sendFile(path.join(__dirname, 'app/public/index.html'));
+});
+
+app.get('/tables', (req, res) => {
+  console.log('tables page requested');
+  res.sendFile(path.join(__dirname, 'app/public/tables.html'));
+});
+
+app.get('/reserve', (req, res) => {
+  console.log('reserve page requested');
+  res.sendFile(path.join(__dirname, 'app/public/reserve.html'));
+});
 
 
 
-let port = process.env.PORT || 3000;
-let app = express();
+let tables = [
+  {name: "john",
+  phone: "8040300099",
+  email: "john@smith.com",
+  customerID: 1},
+  {name: "jacob",
+  phone: "8043002002",
+  email: "john@jacob.com",
+  customerID: 2},
+  {name: "jingelhimer",
+  phone: "8043900909",
+  email: "jingel@himer.com",
+  customerID: 3},
+  {name: "schmitt",
+  phone: "8040990909",
+  email: "schmitt@tee.com",
+  customerID: 4},
+  {name: "john",
+  phone: "80430002000   ",
+  email: "john@smith.com",
+  customerID: 5},
+  {name: "jacob",
+  phone: "8042003000",
+  email: "jacob@john.com",
+  customerID: 6}
+  ];
 
-app.post('/#', function(req, res) {
+app.get('/api/tables', (req, res) => {
+  // console.log('table data requested');
+  // var response = "testing";
+  res.json(tables);
+});
+
+// reserve API call
+app.post('/api/reserve', (req, res) => {
+	console.log('reserve request submitted');
+	console.log(req.body);
+
+  const newReservation = req.body;
+
+  tables.push(newReservation);
+
+  //console.log(tables);
+
+  // Check if user is in the first 5 in list
+  let isBooked;
+  if(tables.length <= 5){
+    isBooked = true;
+  }
+  else{
+    isBooked = false;
+  }
+
+  res.json(isBooked);
 
 });
 
-app.post('/api/tables', function(req, res) {
-    console.log(req.body);
-    console.log("I'm the api tables")
+
+app.post('/api/clear', (req, res) => {
+  console.log('clear all tables');
+  tables = [];
+  res.sendFile(path.join(__dirname, 'app/public/tables.html'));
 });
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+app.post('/api/killreservation', (req, res) => {
+  console.log(req.body.id);
+
+  tables.splice(req.body.id, 1);
+  // console.log(tables);
+  res.json(tables);
 });
 
-app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, () => {
+	console.log(`App listening on PORT ${PORT}`);
 });
-
-app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-});
-
-app.get("/all", function(req, res) {
-    res.json(reservations);
-});
-
-app.listen(port, function() {
-    console.log("App listening on PORT " + port);
-});
-
-// Post is not idempotent: n times, n resources should be created.
-
-
-/*for (let i = 0; i < data.length; i++) { // front end group need to declare var, but for now call it 'i'
-    if i <= 5;
-} else if {
-    i > 5
-    return 'waitingList';
-} else {
-    console.log('Error: Failed execution');
-}*/
